e9656daadc70f264c822ee1c0ae0c1655f558fb2
Brian O'Connor <briandoconnor@gmail.com>
2015-06-03 15:45:20 -0400

    Not really needed anymore but checking in local changes

--- scripts/calc_stats.pl
+++ scripts/calc_stats.pl
@@ -3,16 +3,20 @@ use JSON;
 use Data::Dumper;
 $|++;
 
-# d <- read.csv(file="in_whitelist.tsv", sep="\t", head=TRUE)
-# e <- read.csv(file="out_whitelist.tsv", sep="\t", head=TRUE)
-# perl calc_stats.pl donor_p_150318030103.jsonl master.whitelist.txt in_whitelist.tsv out_whitelist.tsv
+# bin <- read.csv(file="in_whitelist.bwa.tsv", sep="\t", head=TRUE)
+# bout <- read.csv(file="out_whitelist.bwa.tsv", sep="\t", head=TRUE)
+# sin <- read.csv(file="in_whitelist.sanger.tsv", sep="\t", head=TRUE)
+# sout <- read.csv(file="out_whitelist.sanger.tsv", sep="\t", head=TRUE)
+# perl calc_stats.pl donor_p_150319022850.jsonl master.whitelist.txt in_whitelist.bwa.tsv out_whitelist.bwa.tsv in_whitelist.sanger.tsv out_whitelist.sanger.tsv
 
-my ($file, $whitelist, $in_list_output, $out_list_output) = @ARGV;
+my ($file, $whitelist, $in_list_bwa_output, $out_list_bwa_output, $in_list_sanger_output, $out_list_sanger_output) = @ARGV;
 
 my $json = JSON->new->allow_nonref;
 
-open OUTIN, ">$in_list_output" or die;
-open OUTOUT, ">$out_list_output" or die;
+open OUTIN, ">$in_list_bwa_output" or die;
+open OUTOUT, ">$out_list_bwa_output" or die;
+open OUTINS, ">$in_list_sanger_output" or die;
+open OUTOUTS, ">$out_list_sanger_output" or die;
 
 open IN, "<$file" or die;
 
@@ -21,7 +25,7 @@ my $white = {};
 while(<WHITE>) {
   chomp;
   my @a = split /\s+/;
-  $white->{$a[0]}{$a[1]} = 1; 
+  $white->{$a[0]}{$a[1]} = 1;
 }
 close WHITE;
 
@@ -33,12 +37,23 @@ print OUTIN "\n";
 print OUTOUT join("\t", ("Project", "Donor", "Merge", "BWA", "QC", "Download", "TotalNoQC"));
 print OUTOUT "\n";
 
+
+print OUTINS join("\t", ("Project", "Donor", "Sanger"));
+print OUTINS "\n";
+print OUTOUTS join("\t", ("Project", "Donor", "Sanger"));
+print OUTOUTS "\n";
+
 my $i;
 while (<IN>) {
   chomp;
   $i++;
   if ($i%100 == 0) { print "."; }
   my $json_obj = $json->decode($_);
+  #print Dumper($json_obj);
+  my $variant_timing = $json_obj->{variant_calling_results}{sanger_variant_calling}{workflow_details}{variant_timing_metrics}{workflow}{Wall_s};
+  if ($variant_timing == 0 && $_ =~ /SangerPancancerCgpCnIndelSnvStr/) {
+    print Dumper($json_obj); die;
+  }
   my $dcc_project_code = $json_obj->{dcc_project_code};
   my $submitter_donor_id = $json_obj->{submitter_donor_id};
   #print Dumper($json_obj->{normal_specimen}{alignment}{timing_metrics});
@@ -59,8 +74,14 @@ while (<IN>) {
   #print join("\t", ("Merge:", $merge_timing_seconds/3600, "BWA:", $bwa_timing_seconds/3600, "QC:", $qc_timing_seconds/3600, "Download:", $download_timing_seconds/3600, "Total:", $total_hours));
   if ($white->{$dcc_project_code}{$submitter_donor_id}) {
     print OUTIN join("\t", ($dcc_project_code, $submitter_donor_id, $merge_timing_seconds/3600, $bwa_timing_seconds/3600, $qc_timing_seconds/3600, $download_timing_seconds/3600, $total_hours)), "\n";
+    if ($variant_timing > 0) {
+      print OUTINS join("\t", ($dcc_project_code, $submitter_donor_id, $variant_timing/3600)), "\n";
+    }
   } else {
     print OUTOUT join("\t", ($dcc_project_code, $submitter_donor_id, $merge_timing_seconds/3600, $bwa_timing_seconds/3600, $qc_timing_seconds/3600, $download_timing_seconds/3600, $total_hours)), "\n";
+    if ($variant_timing > 0) {
+      print OUTOUTS join("\t", ($dcc_project_code, $submitter_donor_id, $variant_timing/3600)), "\n";
+    }
   }
   $merge_timing_seconds = 0;
   $bwa_timing_seconds = 0;
@@ -71,6 +92,8 @@ print "\nDONE!\n";
 
 close OUTIN;
 close OUTOUT;
+close OUTINS;
+close OUTOUTS;
 close IN;
 
 sub sum_stats {
@@ -81,7 +104,6 @@ sub sum_stats {
     $bwa_timing_seconds += $metrics->{metrics}{bwa_timing_seconds};
     $qc_timing_seconds += $metrics->{metrics}{qc_timing_seconds};
     $download_timing_seconds += $metrics->{metrics}{download_timing_seconds};
-  } 
+  }
   return($merge_timing_seconds, $bwa_timing_seconds, $qc_timing_seconds, $download_timing_seconds);
 }
-
