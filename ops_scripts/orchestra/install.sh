7737602945bdf3c7501fcbd59157f2adbf0a4a40
Niall Byrne <Niall.Byrne@oicr.on.ca>
2015-06-18 16:07:46 -0400

    Install fix

--- ops_scripts/orchestra/install.sh
+++ ops_scripts/orchestra/install.sh
@@ -3,11 +3,11 @@ keyfile="~/.ssh/wei-dkfz.pem"
 
 subnet=`cat ~.orchestra_subnet`
 echo "Installing dependencies ..."
-echo "" > ~.orchestra_subnet
+echo "" > ~/.orchestra_subnet
 sudo apt-get install -y python-pip 2>&1 > /dev/null
 sudo pip install netaddr 2>&1 > /dev/null
-sudo cp orchestra.py /bin/bash/orchestra
-sudo chmod +x /bin/bash/orchestra
+sudo cp orchestra.py /bin/orchestra
+sudo chmod +x /bin/orchestra
 
 python subnet-install.py $subnet $keyfile
 
