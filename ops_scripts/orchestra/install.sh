3631bd48efd90e023a8d4bacada7b13ab8a8b973
Niall Byrne <Niall.Byrne@oicr.on.ca>
2015-06-18 19:56:30 -0400

    Reorg

--- ops_scripts/orchestra/install.sh
+++ ops_scripts/orchestra/install.sh
@@ -10,8 +10,10 @@ subnet=`cat ~/.orchestra_subnet`
 echo "Installing dependencies ..."
 sudo apt-get install -y python-pip 2>&1 > /dev/null
 sudo pip install netaddr 2>&1 > /dev/null
+echo "Installing orchestra CLI ..."
 sudo sudo ln -sf `pwd`/orchestra.py /bin/orchestra
 sudo chmod +x orchestra.py
-
-python subnet-install.py $subnet $keyfile
+echo "Installing remote webservice on all nodes ..."
+python install/subnet-install.py $subnet $keyfilea
+echo "All done."
 
