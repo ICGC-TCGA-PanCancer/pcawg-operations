1c2d642abe94342ed3751ac5c17d45e4a39526d8
Niall Byrne <Niall.Byrne@oicr.on.ca>
2015-06-18 20:38:50 -0400

    Doc changes

--- ops_scripts/orchestra/install/push.sh
+++ ops_scripts/orchestra/install/push.sh
@@ -3,6 +3,9 @@
 # Installs the webservice onto a single worker node, and tests it for functionality
 # Takes an ip address and a key file as arguments
 
+# If you don't want to blast a whole subnet, you can use this to provision
+# individual nodes
+
 # Although Orchestra will work in any posix environment, this install script
 # is Ubuntu only
 
